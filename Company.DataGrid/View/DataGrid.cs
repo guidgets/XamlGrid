27c00c693f680915fcfc1224edf990c3a163ecd9
ddobrev <devnull@localhost>
2009-03-15 12:09:52 +0000

    Removed Examples.Web as redundant. Added automatic creation of columns.

--- Company.DataGrid/View/DataGrid.cs
+++ Company.DataGrid/View/DataGrid.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using Company.DataGrid.Controllers;
 
 namespace Company.DataGrid.View
@@ -29,6 +31,18 @@ namespace Company.DataGrid.View
 		}
 
 		/// <summary>
+		/// Gets or sets a value indicating whether the columns of the <see cref="DataGrid"/> must be 
+		/// automatically created according to the data source.
+		/// </summary>
+		/// <value><c>true</c> if the columns of the <see cref="DataGrid"/> must be automatically created 
+		/// according to the data source; otherwise, <c>false</c>.</value>
+		public bool AutoCreateColumns
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
 		/// Creates or identifies the element used to display a specified item.
 		/// </summary>
 		/// <returns>
@@ -52,11 +66,38 @@ namespace Company.DataGrid.View
 			return item is RowPresenter;
 		}
 
+		/// <summary>
+		/// Prepares the specified element to display the specified item.
+		/// </summary>
+		/// <param name="element">The element used to display the specified item.</param>
+		/// <param name="item">The item to display.</param>
 		protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
 		{
 			base.PrepareContainerForItemOverride(element, item);
 			// TODO: this isn't quite correct (even with a type check); needs more thinking
 			((RowPresenter) element).Columns = this.Columns;
+		}
+
+		/// <summary>
+		/// Invoked when the <see cref="P:System.Windows.Controls.ItemsControl.Items"/> property changes.
+		/// </summary>
+		/// <param name="e">Information about the change.</param>
+		protected override void OnItemsChanged(System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		{
+			base.OnItemsChanged(e);
+			if (this.Items.Count > 0 && this.AutoCreateColumns)
+			{
+				// TODO: possible optimization: do not recreate all columns if the new items are from the same type
+				this.Columns.Clear();
+				// TODO: do not use reflection here; use our data readers when they are ready.
+				foreach (PropertyInfo property in this.Items[0].GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetProperty))
+				{
+					Column column = new Column();
+					column.DataBinding = new Binding(property.Name);
+					column.DataBinding.Mode = property.CanWrite? BindingMode.TwoWay: BindingMode.OneWay;
+					this.Columns.Add(column);
+				}
+			}
 		}
 	}
 }

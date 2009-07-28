db332141f333881eea932bf91c81d597b2849b76
ddobrev <devnull@localhost>
2009-07-28 19:03:01 +0000

    Fixed DataGrid.AutoCreateColumns to work when changed at runtime.

--- Company.DataGrid/View/DataGrid.cs
+++ Company.DataGrid/View/DataGrid.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Reflection;
 using System.Windows;
@@ -13,6 +14,12 @@ namespace Company.DataGrid.View
 	/// </summary>
 	public class DataGrid : ItemsControl
 	{
+		public static readonly DependencyProperty AutoCreateColumnsProperty =
+			DependencyProperty.Register("AutoCreateColumns", typeof(bool), typeof(DataGrid),
+			                            new PropertyMetadata(true, OnAutoCreateColumnsChanged));
+
+		private readonly List<Column> otherColumns;
+
 		/// <summary>
 		/// Represents a control for displaying and manipulating data with a default tabular view.
 		/// </summary>
@@ -22,7 +29,7 @@ namespace Company.DataGrid.View
 
 			// TODO: carefully review this line: it may be more appropriate to initialize the columns elsewhere and/or use another collection
 			this.Columns = new ObservableCollection<Column>();
-			this.AutoCreateColumns = true;
+			this.otherColumns = new List<Column>();
 		}
 
 		/// <summary>
@@ -43,8 +50,14 @@ namespace Company.DataGrid.View
 		/// according to the data source; otherwise, <c>false</c>.</value>
 		public bool AutoCreateColumns
 		{
-			get;
-			set;
+			get
+			{
+				return (bool) this.GetValue(AutoCreateColumnsProperty);
+			}
+			set
+			{
+				this.SetValue(AutoCreateColumnsProperty, value);
+			}
 		}
 
 		/// <summary>
@@ -95,21 +108,51 @@ namespace Company.DataGrid.View
 		protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
 		{
 			base.OnItemsChanged(e);
-			if (e.Action == NotifyCollectionChangedAction.Reset && this.Items.Count > 0 && this.AutoCreateColumns)
+			if (e.Action == NotifyCollectionChangedAction.Reset)
+			{
+				this.CreateColumns();
+			}
+		}
+
+		private void CreateColumns()
+		{
+			if (this.Items.Count > 0 && this.AutoCreateColumns)
 			{
 				// TODO: possible optimization: do not recreate all columns if the new items are from the same type
+				if (this.otherColumns.Count == 0 && this.Columns.Count > 0)
+				{
+					this.otherColumns.AddRange(this.Columns);
+				}
 				this.Columns.Clear();
 				// TODO: do not use reflection here; use our data readers when they are ready.
-				foreach (PropertyInfo property in this.Items[0].GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.GetProperty))
+				foreach (PropertyInfo property in this.Items[0].GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
 				{
 					Column column = new Column();
 					column.Binding = new Binding(property.Name);
-					column.Binding.Mode = property.CanWrite? BindingMode.TwoWay: BindingMode.OneWay;
+					column.Binding.Mode = property.CanWrite ? BindingMode.TwoWay : BindingMode.OneWay;
 					column.Binding.ValidatesOnExceptions = true;
 					column.Binding.NotifyOnValidationError = true;
 					column.DataType = property.PropertyType;
 					this.Columns.Add(column);
 				}
+			}
+		}
+
+		private static void OnAutoCreateColumnsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			DataGrid dataGrid = (DataGrid) d;
+			List<Column> swap = new List<Column>(dataGrid.Columns);
+			dataGrid.Columns.Clear();
+			foreach (Column column in dataGrid.otherColumns)
+			{
+				dataGrid.Columns.Add(column);
+			}
+			dataGrid.otherColumns.Clear();
+			dataGrid.otherColumns.AddRange(swap);
+			swap.Clear();
+			if (dataGrid.AutoCreateColumns && dataGrid.Columns.Count == 0)
+			{
+				dataGrid.CreateColumns();
 			}
 		}
 	}

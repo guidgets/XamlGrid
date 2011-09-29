e0079c2376944180fad326f34ad588f4570665a8
ddobrev <dpldobrev@yahoo.com>
2011-09-29 20:38:08 +0300

    Changed the version from 1.0.0.0 to 0.0.1.0.
    Signed-off-by: ddobrev <dpldobrev@yahoo.com>

--- XamlGrid/Properties/AssemblyInfo.cs
+++ XamlGrid/Properties/AssemblyInfo.cs
@@ -50,8 +50,8 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 
 [assembly: XmlnsPrefix("http://schemas.xamlgrid.com", "xaml")]
 [assembly: XmlnsDefinition("http://schemas.xamlgrid.com", "XamlGrid.Views")]
